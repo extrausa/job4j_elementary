ae2ebf1b931801a3b72b86ad1edece7c49cb5c17
extrausa <d.kirenkov@gmail.com>
2020-09-22 04:56:16 +0300

    0. Операции сравнения [#362920]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+//0. Операции сравнения [#362920]
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+        boolean result = first > second;
+        System.out.println(result);
+        result = first < second;
+        System.out.println(result);
+        result = first == second;
+        System.out.println(result);
+    }
+
+}
+
