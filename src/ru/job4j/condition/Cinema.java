24bb5fee2b8fc685b10515352c72ab8ea6f9c513
extrausa <d.kirenkov@gmail.com>
2020-09-23 11:46:41 +0300

    4.3.1. Оператор && [#362925]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,9 +1,13 @@
 package ru.job4j.condition;
 //4.1.0. Оператор if с блоком else [#362919]
+//4.3.1. Оператор && [#362925]
 public class Cinema {
     public static void main(String[] args) {
         Cinema.access(true);
         Cinema.access(false);
+        boolean allowByParent = true;
+        boolean hasMoney = false;
+        Cinema.permicion(allowByParent, hasMoney);
     }
 
     public static void access(boolean access) {
@@ -15,5 +19,13 @@ public class Cinema {
 
         }
 
+    }
+
+    public static void permicion(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema");
+        } else {
+            System.out.println("I can't");
+        }
     }
 }
