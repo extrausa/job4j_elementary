eb0b587d29ea669e76a4098d1d5e1b335900d3ac
extrausa <d.kirenkov@gmail.com>
2020-09-25 09:14:25 +0300

    5.0. Цикл for. Определение. [#363037]

--- src/ru/job4j/condition/MultipleSwichWeek.java
+++ src/ru/job4j/condition/MultipleSwichWeek.java
@@ -1,4 +1,7 @@
 package ru.job4j.condition;
+
+import javax.swing.*;
+
 //4.6.2. Switch - matcher [#362929]
 public class MultipleSwichWeek {
 
@@ -15,9 +18,72 @@ public class MultipleSwichWeek {
         };
     }
 
+    public static int calculate(int[] hours) {
+        int hoursJob = hours[0] + hours[1] + hours[2] + hours[3] + hours[4];
+        int monday = hours[0];
+        int moneyone = 0;
+        int moneytwo = 0;
+        int holiday = 0;
+        int holidayUp = 0;
+        if (monday <= 8) {
+            moneyone = monday;
+        } else {
+            moneytwo = (monday - 8);
+            moneyone = 8;
+        }
+        int tuesday = hours[1];
+        if (tuesday <= 8) {
+            moneyone += tuesday;
+        } else {
+            moneytwo += (tuesday - 8);
+            moneyone += 8;
+        }
+        int wednesday = hours[2];
+        if (wednesday <= 8) {
+            moneyone += wednesday;
+        } else {
+            moneytwo += (wednesday - 8);
+            moneyone += 8;
+        }
+        int thursday = hours[3];
+        if (thursday <= 8) {
+            moneyone += thursday;
+        } else {
+            moneytwo += (thursday - 8);
+            moneyone += 8;
+        }
+        int friday = hours[4];
+        if (friday <= 8) {
+            moneyone += friday;
+        } else {
+            moneytwo += (friday - 8);
+            moneyone += 8;
+        }
+        int saturday = hours[5];
+        if (saturday <= 8) {
+            holiday = saturday;
+        } else {
+            holidayUp = (saturday - 8);
+            holiday = 8;
+        }
+        int sunday = hours[6];
+        if (sunday <= 8) {
+            holiday += sunday;
+        } else {
+            holidayUp += (sunday - 8);
+            holiday += 8;
+        }
+
+        return (moneyone * 10) + (holiday * 20) + (holidayUp * 30) + (moneytwo * 15);
+
+    }
+
     public static void main(String[] args) {
-        int numberDay = MultipleSwichWeek.numberOfDay("понедельник");
-        System.out.println(numberDay);
+//        int numberDay = MultipleSwichWeek.numberOfDay("понедельник");
+//        System.out.println(numberDay);
+        int[] time = {10, 0, 12, 0, 8, 12, 4};
+        int cash = MultipleSwichWeek.calculate(time);
+        System.out.println(cash);
     }
 
 }
