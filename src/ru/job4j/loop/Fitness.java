44de1112db296506775f946b1318fab42de8e1a4
extrausa <d.kirenkov@gmail.com>
2020-09-26 06:07:04 +0300

    5.4. Протеиновая диета [#363027]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+//5.4. Протеиновая диета [#363027]
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan < nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+        return month;
+    }
+}
