6464f2b643f968598b5f5bc5d91ea41289dd0ea4
extrausa <d.kirenkov@gmail.com>
2020-09-28 09:32:33 +0300

    6.0.1. Объявление массива. [#362967]

--- src/ru/job4j/live/Loop.java
+++ src/ru/job4j/live/Loop.java
@@ -2,8 +2,9 @@ package ru.job4j.live;
 //5.9.1. Генерация конструкций в IDEA [#363034]
 public class Loop {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 0; i++) {
             System.out.println(i);
+
         }
     }
 }
