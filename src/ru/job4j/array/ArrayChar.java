bc5d2505f32f059ab508b4d945a332eb46f0ac13
extrausa <d.kirenkov@gmail.com>
2020-09-30 11:32:29 +0300

    //6.4. Слово начинается с ... [#362940]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,18 +2,17 @@ package ru.job4j.array;
 //6.4. Слово начинается с ... [#362940]
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i < pref.length; i++) {
 //            if (word.length != pref.length) {
 //                result = false;
 //                break;
 //            }
-            if (word[i] == pref[i]) {
-                result = true;
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
 
             }
-            result = false;
-
         }
         return result;
     }
