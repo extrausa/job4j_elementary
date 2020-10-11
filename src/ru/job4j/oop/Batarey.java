037c5c28cb8ae9578dc1ad2af48b96e2fbbaa018
extrausa <d.kirenkov@gmail.com>
2020-10-11 06:23:58 +0300

    1.7. Состояние объекта [#363136]

---
+++ src/ru/job4j/oop/Batarey.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+//1.7. Состояние объекта [#363136]
+public class Batarey {
+    private int load;
+
+    public Batarey(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Batarey another) {
+        this.load = this.load - another.load;
+        another.load = 0;
+
+
+    }
+
+    public static void main(String[] args) {
+        Batarey firstBatarey = new Batarey(100);
+        Batarey secondBatarey = new Batarey(80);
+        System.out.println("firstBatarey : " + firstBatarey.load + ". second : " + secondBatarey.load);
+        firstBatarey.exchange(secondBatarey);
+        System.out.println("firstBatarey : " + firstBatarey.load + ". second : " + secondBatarey.load);
+    }
+}
