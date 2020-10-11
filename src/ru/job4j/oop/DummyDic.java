371db275cbeada5bd9e310fdb8bc530dc2001e16
extrausa <d.kirenkov@gmail.com>
2020-10-11 04:55:55 +0300

    1.4. Вызов метода с возвращаемым типом. [#363135]

---
+++ src/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+//1.4. Вызов метода с возвращаемым типом. [#363135]
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "неизвестное слово" + eng;
+    }
+}
