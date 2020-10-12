3d30a32fea8c790a3576e617d6d2ba43c97f0900
extrausa <d.kirenkov@gmail.com>
2020-10-12 09:57:04 +0300

    2.2. Рефакторинг - Площадь треугольника. [#363127]

---
+++ src/ru/job4j/oop/Triangle.java
@@ -0,0 +1,46 @@
+package ru.job4j.oop;
+//2.2. Рефакторинг - Площадь треугольника. [#363127]
+import static java.lang.Math.sqrt;
+
+import static java.lang.Math.pow;
+
+public class Triangle {
+    private Point first;
+    private  Point second;
+    private  Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        if (ab + ac > bc || ac + bc > ab || bc + ab > ac) {
+            return true;
+        }
+        return false;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = sqrt(p * (p - ab) * (p - ac) * (p - bc));
+
+        }
+        return rsl;
+    }
+
+
+
+
+}
