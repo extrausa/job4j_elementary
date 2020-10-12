cc8c150f3524099946a7d0e2c8acb683c8f6cf07
extrausa <d.kirenkov@gmail.com>
2020-10-12 06:00:47 +0300

    2.1. Рефакторинг - Расстояние между точками. [#363126]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -6,36 +6,36 @@ import org.junit.Assert;
 
 public class PointTest {
 
-    @Test
-    public void when00to20then2() {
-        int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when02to35then424() {
-        double expected = 4.24;
-        int x1 = 0;
-        int y1 = 2;
-        int x2 = 3;
-        int y2 = 5;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when10to42then360() {
-        double expected = 3.60;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 4;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
+//    @Test
+//    public void when00to20then2() {
+//        int expected = 2;
+//        int x1 = 0;
+//        int y1 = 0;
+//        int x2 = 2;
+//        int y2 = 0;
+//        double out = Point.distance(x1, y1, x2, y2);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
+//
+//    @Test
+//    public void when02to35then424() {
+//        double expected = 4.24;
+//        int x1 = 0;
+//        int y1 = 2;
+//        int x2 = 3;
+//        int y2 = 5;
+//        double out = Point.distance(x1, y1, x2, y2);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
+//
+//    @Test
+//    public void when10to42then360() {
+//        double expected = 3.60;
+//        int x1 = 1;
+//        int y1 = 0;
+//        int x2 = 4;
+//        int y2 = 2;
+//        double out = Point.distance(x1, y1, x2, y2);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
 }
