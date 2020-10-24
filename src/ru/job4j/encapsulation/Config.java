7aefc9e6f24e7d60b956d03b2e78dfa14033e483
extrausa <d.kirenkov@gmail.com>
2020-10-24 07:14:37 +0300

    0. Что такое инкапсуляция. [#363160]

---
+++ src/ru/job4j/encapsulation/Config.java
@@ -0,0 +1,23 @@
+package ru.job4j.encapsulation;
+//0. Что такое инкапсуляция. [#363160]
+public class Config {
+    private String name;
+    private int position;
+    private String[] properties;
+
+    public Config(String name) {
+        this.name = name;
+    }
+
+    void print() {
+        System.out.println(position);
+    }
+
+    public String getProperty(String key) {
+        return search(key);
+    }
+
+    private String search(String key) {
+        return key;
+    }
+}
