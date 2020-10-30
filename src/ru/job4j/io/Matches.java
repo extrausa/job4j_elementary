a4f537f9841cd90aca641a22ea2ce877f6660627
extrausa <d.kirenkov@gmail.com>
2020-10-30 06:42:13 +0300

    2. Scanner и чтение числа из консоли. [#363080]

---
+++ src/ru/job4j/io/Matches.java
@@ -0,0 +1,58 @@
+package ru.job4j.io;
+//2. Scanner и чтение числа из консоли. [#363080]
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        int point = 11;
+        int playerOne, playerTwo;
+        Scanner inputPlayerOne = new Scanner(System.in);
+        Scanner inputPlayerTwo = new Scanner(System.in);
+        System.out.println("Берем от 1 до 3 спичек");
+        while (point > 0) {
+            System.out.println("количество спичек = " + point);
+            System.out.println("Ход первого игрока");
+            int selectPlaerOne = Integer.valueOf(inputPlayerOne.nextLine());
+            point -= selectPlaerOne;
+            if (point < 0 || selectPlaerOne > 3) {
+                point += selectPlaerOne;
+                System.out.println("подумай ещё");
+                selectPlaerOne = Integer.valueOf(inputPlayerOne.nextLine());
+                point -= selectPlaerOne;
+                if (point < 0 || selectPlaerOne > 3) {
+                    point += selectPlaerOne;
+                } else if (point == 0) {
+                    System.out.println("победа");
+                    break;
+                }
+            }else if (point == 0) {
+                System.out.println("победа");
+                break;
+            }
+            System.out.println("количество спичек = " + point);
+            System.out.println("ход перешел второму игроку");
+            System.out.println("Второй игрок, ждём твой ход");
+            int selectPlaerTwo = Integer.valueOf(inputPlayerTwo.nextLine());
+            point -= selectPlaerTwo;
+            if (point < 0 || selectPlaerTwo > 3) {
+                point += selectPlaerTwo;
+                System.out.println("подумай ещё");
+                selectPlaerOne = Integer.valueOf(inputPlayerTwo.nextLine());
+                point -= selectPlaerTwo;
+                if (point < 0 || selectPlaerTwo > 3) {
+                    point += selectPlaerTwo;
+                } else if (point == 0) {
+                    System.out.println("победа");
+                    break;
+                }
+            }else if (point == 0) {
+                System.out.println("победа");
+                break;
+            }
+
+        }
+    }
+
+}
