2c629afa4832f987b52cbf914a5216696cf0f59c
extrausa <d.kirenkov@gmail.com>
2020-11-11 13:39:30 +0300

    //0.4. Пользовательские исключения. [#363101]

--- src/ru/job4j/ex/ElementNotFoundException.java
+++ src/ru/job4j/ex/ElementNotFoundException.java
@@ -5,31 +5,7 @@ public class ElementNotFoundException extends Exception {
         super(message);
     }
 
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
-        for (String v: value
-             ) {
-            if (v.equals(key)) {
-                rsl = 1;
-                break;
-            } else {
-                throw new ElementNotFoundException("not found");
-            }
-        }
-        return rsl;
-    }
 
-    public static void main(String[] args) {
-        String[] word = {"car", "dog", "monkey", "elphant"};
-        String transport = "car";
-        String air = "aircraft";
-        try {
-            System.out.println(indexOf(word, transport));
-            System.out.println(indexOf(word ,air));
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 
 
 }
---
+++ src/ru/job4j/ex/FindEI.java
@@ -0,0 +1,31 @@
+package ru.job4j.ex;
+//0.4. Пользовательские исключения. [#363101]
+
+public class FindEI {
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = 1;
+                break;
+            }
+        }
+        if (rsl != 1) {
+            throw new ElementNotFoundException("not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] word = {"car", "dog", "monkey", "elphant"};
+        String transport = "car";
+        String air = "aircraft";
+        try {
+            System.out.println(indexOf(word, transport));
+            System.out.println(indexOf(word ,air));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
