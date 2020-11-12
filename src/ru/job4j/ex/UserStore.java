a97595f50f94c02521d7da6c8749472a5137975c
extrausa <d.kirenkov@gmail.com>
2020-11-12 16:02:45 +0300

    0.5. Иерархия исключений и множественный catch. [#363102]

--- src/ru/job4j/ex/UserStore.java
+++ src/ru/job4j/ex/UserStore.java
@@ -5,40 +5,48 @@ public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
         User name = null;
         for (int i = 0; i < users.length; i++) {
-            if (login.equals(users[i].getUserName()) && users[i].getUserName().length() > 3) {
+            if (login.equals(users[i].getUserName())) {
                 name = users[i];
                 break;
             }
-
         }
         if (name == null) {
-            throw new UserNotFoundException("Not found");
+            throw new UserNotFoundException("Not found user");
         }
-
         return name;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        int a = user.getUserName().length();
-        if (user.isValid() && user.getUserName().length() < 3) {
+        if (!user.isValid()) {
             throw new UserInvalidException("User login invalid");
         }
-
+        if (user.getUserName().length() < 3) {
+            throw new UserInvalidException("Name < 3 characters");
+        }
         return true;
     }
 
-    public static void main(String[] args) throws UserNotFoundException {
+    public static void main(String[] args) {
         User[] users = {
-                new User("Petr Arsentev", true)
+                new User("Petr Arsentev", true),
+                new User("Nikolay Arsentev", false)
         };
-        User user = findUser(users, "Peter Arsentev");
+        //User user = findUser(users, "Pettr Arsentev");
         try {
+            User user = findUser(users, "Petr Arsentev");
+            User user2 = findUser(users, "Nikolay Arsentev");
             if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+            if (validate(user2)) {
                 System.out.println("This user has an access");
             }
         } catch (UserInvalidException e) {
             e.printStackTrace();
+        } catch(UserNotFoundException e) {
+            e.printStackTrace();
         }
+
 
 
     }
