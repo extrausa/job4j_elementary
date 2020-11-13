99a3432ad2a4160832507441b5d0e11ebee3047f
extrausa <d.kirenkov@gmail.com>
2020-11-13 13:26:52 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#363096]

---
+++ test/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tracker;
+//2. Рефакторинг - Шаблон Декоратор для валидатора. [#363096]
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenGoodInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenOutputShowAllItems () {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ShowAllItems(out),
+                new ExitProgramm(out)
+        };
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        new StartUI(out).init(new ValidateInput(out, in), tracker, actions);
+        assertThat(out.toString(), Is.is(
+                "Menu." + System.lineSeparator() + "0. Show" + System.lineSeparator()
+                        + "1. Exit" + System.lineSeparator()));
+    }
+
+
+
+}
