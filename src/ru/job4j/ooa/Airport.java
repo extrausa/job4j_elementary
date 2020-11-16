e441bc5f0a4b1c3cbdac58ebbfafe932b11b8297
extrausa <d.kirenkov@gmail.com>
2020-11-16 09:06:15 +0300

    4. final [#363119]

---
+++ src/ru/job4j/ooa/Airport.java
@@ -0,0 +1,18 @@
+package ru.job4j.ooa;
+//4. final [#363119]
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+        System.out.println("_______________________");
+
+        Airbus airbus2 = new Airbus("A380");
+        System.out.println(airbus2);
+
+        airbus.setName("A380");
+        System.out.println(airbus2);
+    }
+}
+
