adc36b42c7a37bd7aa17a52ef996ce08acc0193f
extrausa <d.kirenkov@gmail.com>
2020-11-25 10:00:55 +0300

    1. Телефонный справочник на базе ArrayList [#363058]

---
+++ src/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,27 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<Person>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список подощедщих пользователей.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<Person>();
+        for (int i = 0; i < persons.size(); i++) {
+            if (persons.get(i).getName().equals(key)) {
+                result.add(persons.get(i));
+            }
+        }
+
+        return result;
+    }
+}
