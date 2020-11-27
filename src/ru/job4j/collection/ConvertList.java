c9889f3cf7e430be7520faa9cfe13e3a3989f9e5
extrausa <d.kirenkov@gmail.com>
2020-11-27 14:59:54 +0300

    5. Конвертация листа массивов в один лист Integer [#363062]

---
+++ src/ru/job4j/collection/ConvertList.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+//5. Конвертация листа массивов в один лист Integer [#363062]
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertList {
+    public static List<Integer> convert(List<int[]> list) {
+        List<Integer> rsl = new ArrayList<>();
+        for (int[] row : list) {
+            for (int cell : row) {
+                rsl.add(cell);
+            }
+        }
+        return rsl;
+    }
+}
