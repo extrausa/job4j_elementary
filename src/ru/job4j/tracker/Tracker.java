719893d48216c2e0f805283f6854b39926335b42
extrausa <d.kirenkov@gmail.com>
2020-11-29 12:28:56 +0300

    6. Изменить программу Tracker из 2-го модуля [#363063]

--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -2,27 +2,37 @@ package ru.job4j.tracker;
 //6. Метод замены заявки. Tracker.replace [#363155]
 //7. Метод удаления заявки Tracker.delete [#363156]
 //8. Что такое валидация? [#363157]
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    //private final Item[] items = new Item[100];
+    private final List<Item> items = new ArrayList<>();
+    //private TrackerSingle trackerSingle = new TrackerSingle();
 
-    private int ads = 1;
-    private int size = 0;
+    private int ads = 0;
+    private int size = items.size();
 
     public Item add(Item item) {
         item.setId(ads++);
-        items[size++] = item;
+        items.add(item);
         return item;
     }
 
     public Item findById(int id) {
         int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return index != -1 ? items.get(index) : null;
+        //return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
-//        Item[] resultArray = new Item[size];
+        Item[] resultArray = new Item[items.size()];
+        int cell = 0;
+        for (Item item : items) {
+            resultArray[cell] = item;
+            cell++;
+        }
 //        int number = 0;
 //        for (int i = 0; i < size; i++) {
 //            Item item = items[i];
@@ -31,29 +41,49 @@ public class Tracker {
 //                number++;
 //            }
 //        }
-        return Arrays.copyOf(items, size);
+        //return Arrays.copyOf(items, size);
+        return resultArray;
+
 
 
     };
 
     public Item[] findByName(String key) {
-        Item[] resultItems = new Item[size];
-        int number = 0;
-        for (int i = 0; i < size; i++) {
-            Item item = items[i];
+        Item[] resultItems = new Item[items.size()];
+        int cell = 0;
+        for (Item item : items) {
             if (item.getName().equals(key)) {
-                resultItems[number] = item;
-                number++;
+                resultItems[cell] = item;
+                cell++;
             }
+
         }
-        return Arrays.copyOf(resultItems,number);
+
+        return resultItems;
+//        int number = 0;
+//        for (int i = 0; i < size; i++) {
+//            Item item = items[i];
+//            if (item.getName().equals(key)) {
+//                resultItems[number] = item;
+//                number++;
+//            }
+//        }
+//        return Arrays.copyOf(resultItems,number);
     }
 
     private int indexOf(int id) {
+//        int rsl = -1;
+//        for (int i = 0; i < size; i++) {
+//            if (items[i].getId() == id) {
+//                rsl = i;
+//                break;
+//            }
+//        }
+//        return rsl;
         int rsl = -1;
-        for (int i = 0; i < size; i++) {
-            if (items[i].getId() == id) {
-                rsl = i;
+        for (Item numb : items) {
+            if (numb.getId() == id) {
+                rsl = numb.getId();
                 break;
             }
         }
@@ -64,22 +94,25 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (index != -1) {
-            items[index] = item;
-            items[index].setId(id);
+            items.set(index, item);
+//            items[index] = item;
+//            items[index].setId(id);
             result = true;
         }
 
         return result;
+
     }
 
     public boolean delete(int id) {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (index != -1){
-            items[index] = null;
-            System.arraycopy(items, index + 1, items, index, size - index);
-            items[size - 1] = null;
-            size--;
+//            items[index] = null;
+//            System.arraycopy(items, index + 1, items, index, size - index);
+//            items[size - 1] = null;
+//            size--;
+            items.remove(index);
         }
 
         return rsl;
