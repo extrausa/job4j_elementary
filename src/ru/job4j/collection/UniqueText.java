7782c397fc4010f0cdccdcc23866b080467df518
extrausa <d.kirenkov@gmail.com>
2020-11-30 14:22:53 +0300

    2. Идентичные тексты [#363072]

---
+++ src/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+//2. Идентичные тексты [#363072]
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String ch: origin) {
+            check.add(ch);
+            
+        }
+
+        for (String st:text) {
+            if (check.contains(st)){
+                rsl = false;
+            }
+
+            
+        }
+
+        return rsl;
+    }
+}
