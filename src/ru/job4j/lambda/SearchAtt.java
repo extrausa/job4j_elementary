9732fd3bb4285a1e341643f06fae23487012150c
extrausa <d.kirenkov@gmail.com>
2020-12-23 13:34:32 +0300

    2.0. Встроенные функциональные интерфейсы. [#362886]

--- src/ru/job4j/lambda/SearchAtt.java
+++ src/ru/job4j/lambda/SearchAtt.java
@@ -2,41 +2,44 @@ package ru.job4j.lambda;
 //2.0. Встроенные функциональные интерфейсы. [#362886]
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 public class SearchAtt {
 
-    private static List<Attachment> filter(UnaryOperator<Attachment> first, List<Attachment> list ){
+    private static List<Attachment> filter(Predicate<Attachment> first, List<Attachment> list ){
         List<Attachment> rsl = new ArrayList<Attachment>();
         for (Attachment att : list) {
-            rsl.add(first.apply(att));
+            if (first.test(att)){
+                rsl.add(att);
+            }
         }
         return rsl;
     }
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        UnaryOperator<Attachment> first = new UnaryOperator<Attachment>() {
+        Predicate<Attachment> first = new Predicate<Attachment>() {
             @Override
-            public Attachment apply(Attachment o) {
-                if (o.getSize() > 100) {
-                    return o;
+            public boolean test(Attachment attachment) {
+                if (attachment.getSize() > 100) {
+                    return true;
                 } else {
-                    o.setName("CSSs");
+                    return false;
                 }
-                return o;
             }
         };
         return filter(first, list);
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-      UnaryOperator<Attachment> first = new UnaryOperator<Attachment>() {
+        Predicate<Attachment> first = new Predicate<Attachment>() {
             @Override
-            public Attachment apply(Attachment o) {
-                if (o.getName().contains("bug")) {
-                    return o;
+            public boolean test(Attachment attachment) {
+                if (attachment.getName().contains("bug")) {
+                    return true;
+                } else {
+                    return false;
                 }
-                return null;
             }
         };
         return filter(first, list);
