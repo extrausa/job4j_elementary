c8ff770f8812ee3fe39f69199d2bbf49bece2cc0
extrausa <d.kirenkov@gmail.com>
2020-12-23 12:42:45 +0300

    2.0. Встроенные функциональные интерфейсы. [#362886]

--- src/ru/job4j/lambda/SearchAtt.java
+++ src/ru/job4j/lambda/SearchAtt.java
@@ -6,44 +6,39 @@ import java.util.function.UnaryOperator;
 
 public class SearchAtt {
 
-    private static List<Attachment> filter(UnaryOperator<Attachment> first ){
-        List<Attachment> rsl = first;
-        for (Attachment att : first) {
-            rsl = first.apply(att);
+    private static List<Attachment> filter(UnaryOperator<Attachment> first, List<Attachment> list ){
+        List<Attachment> rsl = new ArrayList<Attachment>();
+        for (Attachment att : list) {
+            rsl.add(first.apply(att));
         }
-
         return rsl;
-
-
     }
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        List<Attachment> rsl = new ArrayList<Attachment>();
         UnaryOperator<Attachment> first = new UnaryOperator<Attachment>() {
             @Override
             public Attachment apply(Attachment o) {
                 if (o.getSize() > 100) {
-                    return rsl.add(o);
+                    return o;
+                } else {
+                    o.setName("CSSs");
                 }
-                return null;
+                return o;
             }
         };
-        return filter(first);
+        return filter(first, list);
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-        List<Attachment> rsl = new ArrayList<Attachment>();
       UnaryOperator<Attachment> first = new UnaryOperator<Attachment>() {
             @Override
             public Attachment apply(Attachment o) {
                 if (o.getName().contains("bug")) {
-                    return rsl.add(o);
+                    return o;
                 }
                 return null;
             }
         };
-        return filter(first);
+        return filter(first, list);
     }
-
-
 }
