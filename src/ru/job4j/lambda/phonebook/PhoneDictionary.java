b170ee64c9cafeed3613244932febeec82d65299
extrausa <d.kirenkov@gmail.com>
2020-12-26 12:57:03 +0300

    6. Функции высшего порядка [#362879]

--- src/ru/job4j/lambda/phonebook/PhoneDictionary.java
+++ src/ru/job4j/lambda/phonebook/PhoneDictionary.java
@@ -13,6 +13,7 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         Predicate<Person> surname = person -> person.getSurname().contains(key);
         Predicate<Person> name = person -> person.getName().contains(key);
+        Predicate<Person> phone = person -> person.getAddress().contains(key);
         Predicate<Person> combine =  surname.or(name);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
