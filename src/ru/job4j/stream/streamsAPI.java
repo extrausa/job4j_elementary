d878828f52696283b99118b1d71ecc0f4a8854a0
extrausa <d.kirenkov@gmail.com>
2020-12-28 10:25:54 +0300

    0. Stream API [#362892]

--- src/ru/job4j/stream/streamsAPI.java
+++ src/ru/job4j/stream/streamsAPI.java
@@ -3,16 +3,15 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 //0. Stream API [#362892]
 public class streamsAPI {
 
 
 
     public static void main(String[] args) {
-        List<Integer> array = new ArrayList<Integer>();
-        for (int i = 0; i < 20; i++) {
-            array.add(i);
-        }
+        List<Integer> array = IntStream.range(0,21).boxed().collect(Collectors.toList());
         List<Integer> positiveNumbers = array.stream().filter(integer -> integer % 2 == 0).collect(Collectors.toList());
         //positiveNumbers.forEach(n -> System.out.println(n));
         positiveNumbers.forEach(System.out :: println);
