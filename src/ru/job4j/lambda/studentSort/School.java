67ec819a203e1864d7ed8ee3b21f7c5c6cbc022a
extrausa <d.kirenkov@gmail.com>
2020-12-29 06:10:04 +0300

    1. Фильтрация учеников [#362893]

---
+++ src/ru/job4j/lambda/studentSort/School.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda.studentSort;
+//1. Фильтрация учеников [#362893]
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict){
+        List<Student> rsl = students.stream()
+                .filter(student -> predict.test(student))
+                .collect(Collectors.toList());
+        
+
+
+        return rsl;
+    }
+}
