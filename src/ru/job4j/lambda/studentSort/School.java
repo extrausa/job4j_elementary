63d2b82b3bb25da272d8da0377a35f728e013b7d
extrausa <d.kirenkov@gmail.com>
2020-12-31 08:05:05 +0300

    4. Преобразование List в Map. [#362896]

--- src/ru/job4j/lambda/studentSort/School.java
+++ src/ru/job4j/lambda/studentSort/School.java
@@ -2,6 +2,8 @@ package ru.job4j.lambda.studentSort;
 //1. Фильтрация учеников [#362893]
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,5 +12,16 @@ public class School {
         return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, Student> mapCollect(List<Student> students, Predicate<Student> predict){
+        return students.stream()
+                .filter(predict)
+                .distinct().collect(
+                        Collectors.toMap(
+                                Student::getSurname,
+                                Function.identity()
+                                )
+                );
     }
 }
